d3a8307aac76553a1fa7d54a8b52854fa68ab64c
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-10-05 11:45:57 +0300

    feat: add Product.tsx and interface

--- src/components/Main/Product.tsx
+++ src/components/Main/Product.tsx
@@ -1,5 +1,22 @@
-export const Product = () => {
-    return (
-        <></>
-    )
+import React from 'react';
+import { IProduct } from '../../interfaces/IProduct';
+import { Flex } from '../../styles/Flex';
+
+interface ProductProps {
+  product: IProduct;
 }
+
+export const Product = ({ product }: ProductProps) => {
+  return (
+    <Flex direction={'column'} style={{ width: '25vw' }}>
+      <span>{product.id}</span>
+      <p>{product.title}</p>
+      <h3>{product.category}</h3>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <p>{product.rating.rate}</p>
+      <p>{product.rating.count}</p>
+      <img src={product.image} alt={product.title} style={{ width: '10vw' }} />
+    </Flex>
+  );
+};
