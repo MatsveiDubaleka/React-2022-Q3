6b63c284c7d48105efae6a8718ebceb1fafdc814
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-10-05 11:45:30 +0300

    feat: add NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,15 @@
-export const NotFound = () => {
-    return (
-        <></>
-    )
-}
+import React from 'react';
+import { Header } from '../components/Header/Header';
+import { Footer } from '../components/Footer/Footer';
+
+const NotFound = () => {
+  return (
+    <>
+      <Header />
+      <h1>Error: 404 NotFound</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default NotFound;
