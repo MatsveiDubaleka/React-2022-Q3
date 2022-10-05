823067e763ba8f730f8c0db53c379e3586708369
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-10-05 11:45:19 +0300

    feat: add Main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,45 @@
-export const Main = () => {
+import React, { Component, PropsWithChildren } from 'react';
+import { Product } from './Product';
+import { Flex } from '../../styles/Flex';
+import { IProduct } from '../../interfaces/IProduct';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+interface IProducts {
+  products: IProduct[];
+}
+
+export class Main extends Component<PropsWithChildren, IProducts> {
+  constructor(props: PropsWithChildren) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
+
+  render() {
+    const getProducts = async () => {
+      await fetch('https://fakestoreapi.com/products')
+        .then((res) => res.json())
+        .then((json: IProduct[]) => this.setState({ products: json }));
+    };
+
+    getProducts();
     return (
-        <></>
-    )
+      <Flex direction={'column'} gap={'50px'}>
+        <h2>Main</h2>
+        <Flex direction={'row'} style={{ 'flex-wrap': 'wrap' }}>
+          {this.state.products ? (
+            this.state.products.map((product: IProduct, index: number) => {
+              return <Product key={index} product={product} />;
+            })
+          ) : (
+            <Box sx={{ display: 'flex' }}>
+              <CircularProgress />
+            </Box>
+          )}
+        </Flex>
+      </Flex>
+    );
+  }
 }
