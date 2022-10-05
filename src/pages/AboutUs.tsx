719864725fb679938d3927f589b5968fb3f9f0d0
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-10-05 11:38:48 +0300

    refactor: add packages and gitignore

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>What should you know about us? Is Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consequatur ex fuga mollitia quaerat reprehenderit rerum ullam velit veniam. Molestiae?</p>
+    </div>
+  );
+};
+
+export default AboutUs;
