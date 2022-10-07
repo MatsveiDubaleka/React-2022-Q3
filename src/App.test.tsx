611f6af754558108929de4534790dc5ce08a6b70
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-10-07 10:51:51 +0300

    feat: add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { SearchBar } from './components/Header/SearchBar';
+import { Main } from './components/Main/Main';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('SearchBar', () => {
+  test('Finds label search', () => {
+    render(<SearchBar/>)
+    screen.debug()
+    expect(screen.getAllByText(/search/i))
+  });
+})
+
+describe('Main',  () => {
+  test('Finds alt text in images', async() => {
+    render(<Main/>)
+    screen.debug()
+    expect(await screen.findAllByAltText(/mens/i))
+  })
+})
