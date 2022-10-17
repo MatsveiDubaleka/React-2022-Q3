b75c55f0dd14e7b97a86b4b45e40fcad3731cd1d
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-10-17 17:49:34 +0300

    refactor: change location of Searchbar in test and add semicolon Mon, 17 Oct 2022 17:49:34 +0300

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { SearchBar } from '../components/Header/SearchBar';
+import { SearchBar } from '../components/Main/SearchBar';
 import { Main } from '../components/Main/Main';
 
 describe('SearchBar', () => {
   test('Finds label search', () => {
-    render(<SearchBar/>)
-    screen.debug()
-    expect(screen.getAllByText(/search/i))
+    render(<SearchBar />);
+    screen.debug();
+    expect(screen.getAllByText(/search/i));
   });
-})
+});
 
-describe('Main',  () => {
-  test('Finds alt text in images', async() => {
-    render(<Main/>)
-    screen.debug()
-    expect(await screen.findAllByAltText(/mens/i))
-  })
-})
+describe('Main', () => {
+  test('Finds alt text in images', async () => {
+    render(<Main />);
+    screen.debug();
+    expect(await screen.findAllByAltText(/mens/i));
+  });
+});
