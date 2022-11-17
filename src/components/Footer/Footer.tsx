78d9ce10e8220752eb687bacec00ec88fa530ae5
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-17 14:54:39 +0300

    feat: add styles to Footer Thu, 17 Nov 2022 14:54:39 +0300

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import styled from 'styled-components';
+import { Flex } from 'styles/Flex';
+
+const StyledFooter = styled(Flex)`
+  width: 100%;
+  bottom: 0;
+  background: #322e2f;
+  height: 10vh;
+  color: #fff;
+  border-top: 5px solid #f3ca20;
+`;
+
 export const Footer = () => {
   return (
-    <>
-      <h2>Footer</h2>
-    </>
+    <footer style={{ width: '100%' }}>
+      <StyledFooter>
+        <h2>Footer</h2>
+      </StyledFooter>
+    </footer>
   );
 };
