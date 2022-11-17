18619626194f82b5b46655265066b8023f9d7438
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-17 14:54:06 +0300

    feat: add styles to body Thu, 17 Nov 2022 14:54:06 +0300

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { Header } from '../components/Header/Header';
 import { Footer } from '../components/Footer/Footer';
 import { Main } from '../components/Main/Main';
+import styled from 'styled-components';
+import { Flex } from 'styles/Flex';
+
+const StyledPage = styled(Flex)`
+  width: 100%;
+`;
 
 const HomePage = () => {
   return (
-    <>
+    <StyledPage direction="column" align="center" justify="space-between">
       <Header />
       <Main />
       <Footer />
-    </>
+    </StyledPage>
   );
 };
 
