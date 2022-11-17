96b249abf49d9ab29e3a3ba7ab750a67b771b471
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-11-17 13:27:21 +0300

    feat: add Context.Provider to App.tsx Thu, 17 Nov 2022 13:27:42 +0300

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,18 @@ import { Route, Routes } from 'react-router-dom';
 import AboutUs from './pages/AboutUs';
 import NotFound from './pages/NotFound';
 import { CreateCard } from './pages/CreateCard';
+import { Context } from 'GlobalContext';
 
 function App() {
   return (
-    <>
+    <Context.Provider value={{ title: '256' }}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/card" element={<CreateCard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </Context.Provider>
   );
 }
 
